ec85a448df801389f369fddc5c2fe4986c9dc6c3
egardea <erik1gardea@gmail.com>
2020-02-06 20:39:12 -0600

    added the pages to state of the profile reducer

--- src/Reducers/MovieReducers/MovieFavorite.js
+++ src/Reducers/MovieReducers/MovieFavorite.js
@@ -2,16 +2,20 @@ import {MOVIE_FAVORITE} from '../../Actions/Types';
 
 const initialState = {
     favMovies: [],
+    page: 1
 }
 
 export default function (state = initialState, action) {
     const newItem = action.payload;
     const favMovies = state.favMovies.slice();
+    const page = Math.ceil((state.favMovies.slice().length + 1) / 8);
+
     favMovies.push(newItem);
     switch(action.type) {
         case MOVIE_FAVORITE:
             return {
                 favMovies,
+                page,
             }   
         default: 
             return state;
