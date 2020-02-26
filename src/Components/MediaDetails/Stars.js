d45e06b21b19a4a66021f3e1a4e65476cacd2a94
egardea <erik1gardea@gmail.com>
2020-02-25 20:57:39 -0600

    commented the code

--- src/Components/MediaDetails/Stars.js
+++ src/Components/MediaDetails/Stars.js
@@ -4,7 +4,9 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 const Stars = (props) => {
 
+    //takes in the number rating from props, since we only want to display 5 stars. We divide by 2
     const rating = Math.round(props.rating/2);
+    //Loop through the node list until the index is less than the rating and add the active class style for the change of color
     document.querySelectorAll('.star-rating-container-item').forEach((cur, i) => {
         if(i < rating){
             cur.classList.add('star-rating-container-item-active');
